4ed782c2ba1ff14c3a39c86b019758468e43d25f
WosLovesLife <zhang00h00@126.com>
2018-11-03 16:54:25 +0800

    try to handle the gesture events but failed

--- lib/photo_view.dart
+++ lib/photo_view.dart
@@ -13,6 +13,9 @@ class PhotoView extends StatefulWidget {
   State<StatefulWidget> createState() => new _LoadMoreViewState();
 }
 
+const double kMinScale = 1.0;
+const double kMaxScale = 3.0;
+
 class _LoadMoreViewState extends State<PhotoView> with TickerProviderStateMixin {
   GlobalKey _imageKey = new GlobalKey();
 
@@ -22,14 +25,16 @@ class _LoadMoreViewState extends State<PhotoView> with TickerProviderStateMixin 
   Tween<Offset> _positionTween;
 
   // 放大/和放大的基点的值. 在动画/手势中会实时变化
-  double _scale = 1.0;
+  double _scale = kMinScale;
   Offset _position = Offset.zero;
 
   // ==== 辅助动画/手势的计算
-  Offset _downPoint;
+  int _lastTapTime = 0;
+  Offset _downPoint = Offset.zero;
+  Offset _downPosition = Offset.zero;
 
   /// 上次放大的比例, 用于帮助下次放大操作时放大的速度保持一致.
-  double _lastScaleValue = 1.0;
+  double _lastScaleValue = kMinScale;
   Offset _lastPosition = Offset.zero;
 
   @override
@@ -43,17 +48,17 @@ class _LoadMoreViewState extends State<PhotoView> with TickerProviderStateMixin 
   }
 
   _forwardAnimations() {
-    _scaleTween = new Tween<double>(begin: _scale, end: 3.0);
+    _scaleTween = new Tween<double>(begin: _scale, end: kMaxScale);
 
     var containerSize = MediaQuery.of(context).size;
     var center = new Offset(containerSize.width, containerSize.height) / 2.0;
     var delta = center - _downPoint;
-    var positionDelta = (delta * 3.0);
+    var positionDelta = (delta * kMaxScale);
     _positionTween = new Tween<Offset>(begin: _position, end: _clampPosition(positionDelta, 3.0));
   }
 
   _resetAnimations() {
-    _scaleTween = new Tween<double>(begin: _scale, end: 1.0);
+    _scaleTween = new Tween<double>(begin: _scale, end: kMinScale);
     _positionTween = new Tween<Offset>(begin: _position, end: Offset.zero);
   }
 
@@ -88,25 +93,46 @@ class _LoadMoreViewState extends State<PhotoView> with TickerProviderStateMixin 
     return new Offset(computedX, computedY);
   }
 
+  // ======== handle gesture
+
+  _handleDoubleTap() {
+    if (_scale > kMinScale) {
+      _resetAnimations();
+    } else {
+      _forwardAnimations();
+    }
+
+    _controller.reset();
+    _controller.forward();
+  }
+
   _handleScaleStart(ScaleStartDetails details) {
     _downPoint = details.focalPoint;
     _lastScaleValue = _scale;
     _lastPosition = details.focalPoint - _position;
+    _downPosition = _position;
+    // 计算出当前图像的中心点距离屏幕中心点的距离
   }
 
   _handleScaleUpdate(ScaleUpdateDetails details) {
     double newScale = (_lastScaleValue * details.scale);
 
-    if (newScale < 0.7) {
-      newScale = 0.7;
-    } else if (newScale > 5.0) {
-      newScale = 5.0;
+    if (newScale < kMinScale) {
+      newScale = kMinScale;
+    } else if (newScale > kMaxScale) {
+      newScale = kMaxScale;
     }
+
+    var deltaScale = _lastScaleValue - newScale;
+    var ratio = deltaScale / (kMaxScale - kMinScale);
+    var dx = _downPosition.dx - ratio * _downPosition.dx;
+    var dy = _downPosition.dy - ratio * _downPosition.dy;
 
     final Offset positionDelta = (details.focalPoint - _lastPosition);
 
     setState(() {
       _scale = newScale;
+      _position = _clampPosition(Offset(dx, dy), _scale);
 
       // 表示没有缩放操作才响应平移操作
       if (_lastScaleValue == _scale) {
@@ -115,22 +141,7 @@ class _LoadMoreViewState extends State<PhotoView> with TickerProviderStateMixin 
     });
   }
 
-  _checkAndReset() {
-    if (_scale > 3.0) {
-      _scaleTween = new Tween<double>(begin: _scale, end: 3.0);
-      _positionTween = new Tween<Offset>(begin: _position, end: _position);
-      _controller.reset();
-      _controller.forward();
-    } else if (_scale < 1.0) {
-      _resetAnimations();
-      _controller.reset();
-      _controller.forward();
-    }
-  }
-
-  _handleScaleEnd(ScaleEndDetails details) {
-    _checkAndReset();
-  }
+  _handleScaleEnd(ScaleEndDetails details) {}
 
   @override
   void dispose() {
@@ -145,56 +156,50 @@ class _LoadMoreViewState extends State<PhotoView> with TickerProviderStateMixin 
       ..scale(_scale, _scale, 1.0);
 
     return new GestureDetector(
-      onPanDown: (DragDownDetails details) {
+      onTapDown: (TapDownDetails details) {
         _downPoint = details.globalPosition;
         _controller.stop();
-      },
-      onPanCancel: () {
-        _checkAndReset();
-      },
-      child: new GestureDetector(
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-        onDoubleTap: () {
-          if (_scale > 1.0) {
-            _resetAnimations();
-          } else {
-            _forwardAnimations();
-          }
 
-          _controller.reset();
-          _controller.forward();
-        },
-        onScaleStart: _handleScaleStart,
-        onScaleUpdate: _handleScaleUpdate,
-        onScaleEnd: _handleScaleEnd,
-        child: new Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.black,
-          alignment: Alignment.center,
-          child: new Wrap(
-            children: <Widget>[
-              new Transform(
-                transform: transform,
-                alignment: Alignment.center,
-                child: IgnorePointer(
-                  ignoringSemantics: true,
-                  child: new CachedNetworkImage(
-                    imageUrl: widget.imageUrl,
-                    key: _imageKey,
-                    placeholder: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                    errorWidget: Center(
-                      child: new Icon(Icons.image, size: 56.0),
-                    ),
+        // Handle the gesture as a Double tap if the time between the double tap was in 500ms
+        var tapTime = DateTime.now().millisecondsSinceEpoch;
+        if (tapTime - _lastTapTime < 500) {
+          _lastTapTime = 0;
+          _handleDoubleTap();
+        } else {
+          _lastTapTime = tapTime;
+        }
+      },
+      onTap: () {
+//          Navigator.of(context).pop();
+      },
+      onScaleStart: _handleScaleStart,
+      onScaleUpdate: _handleScaleUpdate,
+      onScaleEnd: _handleScaleEnd,
+      child: new Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: new Wrap(
+          children: <Widget>[
+            new Transform(
+              transform: transform,
+              alignment: Alignment.center,
+              child: IgnorePointer(
+                ignoringSemantics: true,
+                child: new CachedNetworkImage(
+                  imageUrl: widget.imageUrl,
+                  key: _imageKey,
+                  placeholder: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: Center(
+                    child: new Icon(Icons.image, size: 56.0),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
